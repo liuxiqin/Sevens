0ff9c750c407300df34a4352209bea66a8210abd
liuxiqin.0301@163.com <liuxiqin.0301@163.com>
2016-03-16 08:58:14 +0800

    sevens domain

---
+++ src/Sevens/Seven/Domains/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Domains
+{
+    /// <summary>
+    /// 实体
+    /// </summary>
+    public abstract class Entity
+    {
+
+    }
+}
---
+++ src/Sevens/Seven/Domains/ValueObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seven.Domains
+{
+    /// <summary>
+    /// 值对象
+    /// </summary>
+    public class ValueObject
+    {
+
+    }
+}
